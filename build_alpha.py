b459a953d2bd6f0d556251308696356825c5ee19
ilyab <i-baykov@internet.ru>
2023-09-28 13:46:50 +0300

    Добавил строку с документацией по каждой функции

--- build_alpha.py
+++ build_alpha.py
@@ -3,4 +3,8 @@ from normolize_func import normalize
 
 
 def build_alpha(vector: list) -> list:
+    """"
+        Заполняю массив в генераторе по формуле -close(i) ( d - 1) / close(i) ( d - 6 )
+        Для полученного массива применяю поочерёдно нейтрализацию и нормализацию
+    """
     return normalize(neutralization([-1 * vector[i - 1] / vector[i - 6] for i in range(6, len(list))]))
